ee31b7c44c190e9677b4a460e04534b4f82a92e8
Khalil Tizaoui <62132130+TizaouiKha@users.noreply.github.com>
2023-03-24 08:08:34 +0100

    v2 fonctionnel

--- src/Controlers/CtrlLecon.java
+++ src/Controlers/CtrlLecon.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -69,20 +70,24 @@ public class CtrlLecon {
         }
         return lesLecons;
     }
-    public ArrayList<Lecon> getAllLeconByIdMoniteurAndByIdEleve(int idMoniteur, int idEleve){
+    
+    public ArrayList<Lecon> getAllLeconByIdMoniteurAndByIdEleve(int idEleve, int idMoniteur){
         ArrayList<Lecon>lesLecons = new ArrayList<>();
         try {
-            ps= cnx.prepareStatement("SELECT CodeLecon, MONTH(Date) AS mois, DAY(Date) as jour, Heure , moniteur.Nom as nomMoniteur, CodeEleve, Immatriculation"
-                    + "FROM lecon "
-                    + "join eleve on lecon.CodeEleve = eleve.CodeEleve "
-                    + "join moniteur on lecon.CodeMoniteur = moniteur.CodeMoniteur "
-                    + "WHERE lecon.CodeMoniteur = ? "
-                    + "AND lecon.CodeEleve = ?;");
-            ps.setInt(1, idMoniteur);
-            ps.setInt(2, idEleve);
-            rs= ps.executeQuery();
+            ps= cnx.prepareStatement("""
+                                     SELECT lecon.CodeLecon,MONTH(lecon.Date) AS mois, DAY(lecon.Date) as jour, lecon.Heure , moniteur.Nom as nomMoniteur, lecon.Immatriculation, eleve.Nom as nomEleve
+                                     FROM lecon 
+                                     join vehicule on lecon.Immatriculation = vehicule.Immatriculation
+                                     join categorie on vehicule.CodeCategorie = categorie.CodeCategorie
+                                     join eleve on lecon.CodeEleve = eleve.CodeEleve
+                                     join moniteur on lecon.CodeMoniteur = moniteur.CodeMoniteur
+                                     WHERE lecon.CodeEleve = ?
+                                     AND lecon.CodeMoniteur=?;""");
+            ps.setInt(1, idEleve);
+            ps.setInt(2, idMoniteur);
+           rs= ps.executeQuery();
             while(rs.next()){
-                Lecon lecon = new Lecon(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7));
+                Lecon lecon = new Lecon(rs.getInt("lecon.CodeLecon"), rs.getString("mois"), rs.getString("jour"), rs.getString("lecon.Heure"), rs.getString("nomMoniteur"), rs.getString("nomEleve"), rs.getString("lecon.Immatriculation"));
                 lesLecons.add(lecon);
             }   
             ps.close();
@@ -92,6 +97,7 @@ public class CtrlLecon {
         }
         return lesLecons;
     }
+    
     public ArrayList<Lecon> getAllLeconByIdEleveByTypePermis(int idEleve){
          ArrayList<Lecon>lesLecons= new ArrayList<>();
         try {
@@ -141,9 +147,20 @@ public class CtrlLecon {
         return lesLecons;
     }
     
-    public void insertLecon(){
-        
-        
+    public void insertLecon(Date date, String heure, int idMoniteur, int idEleve, String immatricualtion, int reglee){
+        try {
+            ps = cnx.prepareStatement("Insert into lecon(Date, Heure, CodeMoniteur, CodeEleve, Immatriculation, Reglee) Values(?,?,?,?,?,?)");
+            ps.setDate(1, new java.sql.Date(date.getTime()));
+            ps.setString(2, heure);
+            ps.setInt(3, idMoniteur);
+            ps.setInt(4, idEleve);
+            ps.setString(5, immatricualtion);
+            ps.setInt(6, reglee);
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(CtrlLecon.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }   
  
 }
